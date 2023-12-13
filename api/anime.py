4cbef46c7c060a9fc91c18e4baf5e7d578bb02bd
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2023-12-13 06:32:56 -0500

    Update anime.py

--- api/anime.py
+++ api/anime.py
@@ -3,35 +3,28 @@ from bs4 import BeautifulSoup
 from api.proxy import Random_Proxy
 
 def getAnime(host, query, page, proxie):
-    moviesDictionary = {'Status': True,'Query': query,'Results': []}
+    moviesDictionary = {'Results': []}
     proxy = Random_Proxy()
     try:
+        base_url = f'https://{host}/filter?keyword={query}'
+
+        if page != None:
+            base_url = f'https://{host}/filter?keyword={query}&page={page}'
+
         if proxie == 'true':
-            if page != None:
-                base_url = f'https://{host}/filter?keyword={query}&page={page}'
-                currentPage = page
-                r = proxy.Proxy_Request(url=base_url, request_type='get')
-                soup = BeautifulSoup(r.content, 'lxml')
-            else:
-                base_url = f'https://{host}/filter?keyword={query}'
-                currentPage = '1'
-                r = proxy.Proxy_Request(url=base_url, request_type='get')
-                soup = BeautifulSoup(r.content, 'lxml')
+            currentPage = page or '1'
+            r = proxy.Proxy_Request(url=base_url, request_type='get')
+            soup = BeautifulSoup(r.content, 'lxml')
         else:
-            if page != None:
-                base_url = f'https://{host}/filter?keyword={query}&page={page}'
-                currentPage = page
-                soup = BeautifulSoup(requests.get(base_url).content, 'lxml')
-            else:
-                base_url = f'https://{host}/filter?keyword={query}'
-                currentPage = '1'
-                soup = BeautifulSoup(requests.get(base_url).content, 'lxml')
+            currentPage = page or '1'
+            soup = BeautifulSoup(requests.get(base_url).content, 'lxml')
+            
     except requests.exceptions.RequestException as e:
         moviesDictionary['Status'] = False,
         moviesDictionary['error'] = str(e),
         return moviesDictionary
 
-    moviesDictionary['Page'] = currentPage
+    moviesDictionary['Current_Page'] = currentPage
     items = soup.find_all('div', class_='item')
 
     for item in items:
@@ -53,5 +46,22 @@ def getAnime(host, query, page, proxie):
         moviesDictionary['Results'].append(moviesObject)
    
     return moviesDictionary
+
+
+def getPages(soup, query):
+    try:
+        ul = soup.find('ul', class_='pagination')
+        li = ul.find_all('li')
+    except:
+        pages = '1'
+        return pages
+
+    for l in li:
+        a = l.find('a', text='»')
+    if a != None:
+        href = a['href']
+        hrefSplit = href.split('page=')
+        pages = hrefSplit[1]
+        return pages
 
  #'Quality': quality, 'Duration': duration,'Cover': poster,'Year': year, 'Content-Type': ctype}
