d8d8af3b11c58a1bfc8ad3da5d7a4bd03785b937
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2023-12-15 14:33:32 -0500

    Update fmovies.py
    Added trending, removed useless call for query when calling pages

--- api/fmovies.py
+++ api/fmovies.py
@@ -42,12 +42,57 @@ def getMovies(host, query, page, proxie):
             link = str(e) 
        
         moviesObject = {'link': link, "Title": Title, 'Quality': quality, 'Cover': poster, "Content-Type": Type} 
-        moviesDictionary['Last_Page'] = getPages(soup, query)
+        moviesDictionary['Last_Page'] = getPages(soup)
         moviesDictionary['Results'].append(moviesObject)
    
     return moviesDictionary
 
-def getPages(soup, query):
+def getTrending(host, page, proxie):
+    moviesDictionary = {'Results': []}
+    proxy = Random_Proxy()
+    try:
+        base_url = f'https://{host}/trending'
+
+        if page != None:
+            base_url = f'https://{host}/trending?page={page}'
+
+        if proxie == 'true':
+            currentPage = page or '1'
+            r = proxy.Proxy_Request(url=base_url, request_type='get')
+            soup = BeautifulSoup(r.content, 'lxml')
+        else:
+            currentPage = page or '1'
+            soup = BeautifulSoup(requests.get(base_url).content, 'lxml')
+            
+    except requests.exceptions.RequestException as e:
+        moviesDictionary['Status'] = False,
+        moviesDictionary['error'] = str(e),
+        return moviesDictionary
+
+    moviesDictionary['Current_Page'] = currentPage 
+    items = soup.find_all('div', class_='item')
+
+    for item in items:
+        try:
+            quality = item.find('div', class_="quality").text
+            pclass = item.find('div', class_='poster')
+            href = pclass.find('a').get('href')
+            link = f'https://{host}{href}'       
+            img = item.find('img')
+            poster = img['data-src']
+            meta = item.find('div', class_="meta")
+            Title = meta.find('a').text
+            Type = item.find('span', class_="type").text
+        except Exception:
+            link = str(e) 
+       
+        moviesObject = {'link': link, "Title": Title, 'Quality': quality, 'Cover': poster, "Content-Type": Type} 
+        moviesDictionary['Last_Page'] = getPages(soup)
+        moviesDictionary['Results'].append(moviesObject)
+   
+    return moviesDictionary
+
+def getPages(soup):
     try:
         ul = soup.find('ul', class_='pagination')
         li = ul.find_all('li')
